c3d321fdded6563870f9d98da7fa5960000b305a
Damon Oehlman <damon.oehlman@gmail.com>
2014-10-24 11:30:01 +1100

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+var reHttpUrl = /^http(.*)$/;
+
+/**
+  # wsurl
+
+  Given a standard `http(s)` or protocol relative url, generate an appropriate
+  url for a WebSocket endpoint (`ws(s)`).
+
+  ## Example Usage
+
+  <<< examples/relative.js
+
+**/
+
+module.exports = function(url, opts) {
+  var current = (opts || {}).current || (typeof location != 'undefined' && location.href);
+  var currentProtocol = current && current.slice(0, current.indexOf(':'));
+  var insecure = (opts || {}).insecure;
+  var isRelative = url.slice(0, 2) == '//';
+  var forceWS = (! currentProtocol) || currentProtocol === 'file:';
+
+  if (isRelative) {
+    return forceWS ?
+      ((insecure ? 'ws:' : 'wss:') + url) :
+      (currentProtocol.replace(reHttpUrl, 'ws$1') + ':' + url);
+  }
+
+  return url.replace(reHttpUrl, 'ws$1');
+};
